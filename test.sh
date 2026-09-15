aee835ce26789354d8d246970043ea76264815da
glabcc <glabcc8@163.com>
2026-09-15 23:25:34 +0800

    demo

---
+++ test.sh
@@ -0,0 +1,5 @@
+# python run.py --execution-provider cpu -s demo/face.jpg -t demo/origin_video.mp4 -o demo/output.mp4
+
+
+python run.py --execution-provider coreml -s demo/face.jpg -t demo/origin_video.mp4 -o demo/output.mp4
+
